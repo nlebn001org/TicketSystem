fa5a9839cd43e393e09c6192c60ea2708408dd7d
Nikita Lebedenko <nlebn001@gmail.com>
2022-05-08 18:33:12 +0200

    Password Encr should be added (account controller + admin changePassword method)

--- TicketSystem/Controllers/AccountController.cs
+++ TicketSystem/Controllers/AccountController.cs
@@ -29,10 +29,94 @@ namespace TicketSystem.Web.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Login(LoginModel model)
+        //{
+
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        User user = await _db.Users.Include(u => u.Role)
+        //            .FirstOrDefaultAsync(u => model.Username == u.Username && model.Password == u.Password);
+
+        //        if (user != null)
+        //        {
+        //            await Authenticate(user); //authentication
+
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //        ModelState.AddModelError("", "Incorrect Username or Password");
+        //    }
+        //    return View(model);
+        //}
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Register(RegisterModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        User user = await _db.Users.FirstOrDefaultAsync(u => u.Username == model.Username);
+        //        if (user == null)
+        //        {
+        //            User newUser = new User { Username = model.Username, Password = model.Password }; //could be registered only customer
+        //            Role userRole = await _db.Roles.FirstOrDefaultAsync(r => r.RoleName == "customer");
+        //            if (userRole != null) newUser.Role = userRole;
+        //            await _db.Users.AddAsync(newUser);
+        //            await _db.SaveChangesAsync();
+        //            await Authenticate(newUser); //authentication
+        //            return RedirectToAction("Index", "Home"); // home index
+        //        }
+        //        else
+        //        {
+        //            ModelState.AddModelError("", "Probably user with this username is already exists.");
+        //        }
+        //    }
+        //    return View(model);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> MyAccount(MyAccountModel accountModel)
+        //{
+        //    User user = await _db.Users.Include(u => u.Role).FirstOrDefaultAsync(u => u.Username == accountModel.Username);
+        //    accountModel.Role = user.Role;
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (user != null)
+        //        {
+        //            if (!string.IsNullOrEmpty(accountModel.NewPassword) || !string.IsNullOrEmpty(accountModel.OldPassword) ||
+        //                !string.IsNullOrEmpty(accountModel.ConfirmPassword))
+        //            {
+        //                if (user.Password != accountModel.OldPassword)
+        //                    ModelState.AddModelError("", "You must enter your previous password for change");
+        //                if (accountModel.NewPassword.Length < 3)
+        //                    ModelState.AddModelError("", "Your password must not be empty or less than 3 symbols.");
+        //                else
+        //                    user.Password = accountModel.NewPassword;
+        //            }
+
+        //            user.Email = string.IsNullOrEmpty(accountModel.Email) ? user.Email : accountModel.Email;
+        //            user.Name = string.IsNullOrEmpty(accountModel.Name) ? user.Name : accountModel.Name;
+        //            user.Surname = string.IsNullOrEmpty(accountModel.Surname) ? user.Surname : accountModel.Surname;
+        //            user.DateChanged = DateTime.Now;
+        //            _db.Update(user);
+        //            await _db.SaveChangesAsync();
+        //        }
+        //        else
+        //            ModelState.AddModelError("", "User does not exist.");
+        //    }
+        //    return View(accountModel);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel model)
         {
+            string encryptedPassword = Encrypt(model.Password);
+
             if (ModelState.IsValid)
             {
                 User user = await _db.Users.Include(u => u.Role)
@@ -119,7 +203,7 @@ namespace TicketSystem.Web.Controllers
                         else
                             user.Password = accountModel.NewPassword;
                     }
-                    
+
                     user.Email = string.IsNullOrEmpty(accountModel.Email) ? user.Email : accountModel.Email;
                     user.Name = string.IsNullOrEmpty(accountModel.Name) ? user.Name : accountModel.Name;
                     user.Surname = string.IsNullOrEmpty(accountModel.Surname) ? user.Surname : accountModel.Surname;
@@ -165,6 +249,34 @@ namespace TicketSystem.Web.Controllers
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login", "Account");
+        }
+
+        static string Encrypt(string password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+
+
+        static string Decrypt(string encryptedPassword)
+        {
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            System.Text.Decoder utf8Decode = encoder.GetDecoder();
+            byte[] todecode_byte = Convert.FromBase64String(encryptedPassword);
+            int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+            char[] decoded_char = new char[charCount];
+            utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+            string result = new string(decoded_char);
+            return result;
         }
 
     }
