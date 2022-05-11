150696872f5820fb61eae2a5b4a04279c26fbf10
Nikita Lebedenko <nlebn001@gmail.com>
2022-05-11 16:17:16 +0200

    continuing with front

--- TicketSystem/wwwroot/js/JavaScript.js
+++ TicketSystem/wwwroot/js/JavaScript.js
@@ -1,12 +1,4 @@
-﻿////window.onload = function print() {
-////    document.getElementById("test").innerHTML = "test";
-////}
-
-window.onload = function setSize() {
-    document.getElementById("render").style.marginTop = document.getElementById("mainNavbar").style.height;
-}
-
-
+﻿
 window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
 
