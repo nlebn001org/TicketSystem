e14d402ec0d41043fac8edb2f8a45d2f55f94d60
Nikita Lebedenko <64488460+nlebn001@users.noreply.github.com>
2022-05-19 17:04:29 +0200

    delete
    Deleted

--- MinimalWebAPI/MinimalWebAPI/Data/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MinimalWebAPI.Data
-{
-    public class Role
-    {
-        public int Id { get; set; }
-        public string? RoleName { get; set; }
-        public List<User>? Users { get; set; }
-    }
-}
